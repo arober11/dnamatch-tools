4eefeca67f47d88e59b51669fec9685603f5d5b9
Andrew Roberts <aroberts@arcalot.net>
2023-07-29 13:38:54 +0100

    substitute ';' for '-' in mutaion names

--- shell/get_YDNA_rsid.sh
+++ shell/get_YDNA_rsid.sh
@@ -32,7 +32,7 @@ then
   rm -f $YDNA_RSIDS $YDNA_MUTS $YDNA_HAPGRP_MUTS $YDNA_RSID_MUTS
 
   #Tidy a couple of Haplogroup names
-  $SED -i -e 's/#REF!/C1a2b1b2/' -e 's/^"A9832,2"/A9832.2/' -e 's/^",//' -e 's/["]//g' $YDNA_SNPS
+  $SED -i -e 's/#REF!/C1a2b1b2/' -e 's/^"A9832,2"/A9832.2/' -e 's/^",//' -e 's/["]//g' -e 's/; /-/g' $YDNA_SNPS
 
   $SED -e '/->/!d' -e 's/^[[:space:]]+//g' -e '/^$/d' -e 's/->/,/' $YDNA_SNPS | perl -F, -ane '$names="$F[0]-$F[2]" ; $names =~ s/[-]+$// ; $type=0 ; $rsid= "" ; if ($F[3] ne "") { $rsid=",\"rsid\":\"$F[3]\""; } $F[7] =~ s/\n//g ; $descendant=$F[7] ; if($descendant =~ /^del/){$descendant="";$type=3;} if($descendant =~ /^ins/){$descendant="";$type=4;} print "$F[1],\"mutations\":[{\"posStart\":\"$F[4]\",\"ancestral\":\"$F[6]\",\"descendant\":\"$descendant\",\"type\":\"$type\",\"display\":\"$F[6]$F[4]$F[7]\",\"label\":\"$names\"$rsid}]\n";' | sort -t, -k1 > $YDNA_HAPGRP_MUTS
   cut -s -d, -f1,4,7 $YDNA_SNPS   | $SED -e 's/^[[:space:]]+//g' -e '/^$/d' -e '/^,/d' -e '/,,/d' | grep 'rs' | sed 's/,/ /g' | perl -ane 'print "$F[1],$F[2],$F[0]\n"' | sort -t, -k1,3 -u > $YDNA_RSID_MUTS
